8bee75e318161dc2444802e7c8f8423635b638ad
Emeka Okoli <emekaokoli00@gmail.com>
2021-05-03 02:35:24 +0100

    created task component

---
+++ src/components/Tasks.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import { TaskList } from "./TaskList";
+
+export const Tasks = () => {
+  return (
+    <div className="h-96 rounded-md">
+      <TaskList />
+    </div>
+  );
+};
