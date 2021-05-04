81b92e98d70dd0d6dbb5de89e38795bd9b4839eb
Emeka Okoli <emekaokoli00@gmail.com>
2021-05-04 10:05:03 +0100

    updated memorytask services

--- src/services/memory/MemoryTaskService.js
+++ src/services/memory/MemoryTaskService.js
@@ -1,16 +1,18 @@
-import { tasks } from "./TaskMock";
-import { Subtask, Task } from "../../models/Task";
-import { TaskService } from "../TaskService";
+import { Subtask, Task } from '../../models/Task';
+import { TaskService } from '../TaskService';
+import { tasks } from './TaskMock';
 
 const taskList = [...tasks];
 
 let autoId = 1001;
 
 export default new TaskService({
-  fetchAllTasks: () => taskList,
-  fetchTaskById: (id) => taskList.find((t) => t.id === id),
+  fetchAllTasks: () => Promise.resolve(taskList),
+  fetchTaskById: (id) => Promise.resolve(taskList.find((t) => t.id === id)),
   createTask: (title) =>
-    taskList.push(new Task({ title, created_at: new Date(), id: autoId++ })),
+    Promise.resolve(
+      taskList.push(new Task({ title, created_at: new Date(), id: autoId++ })),
+    ),
   updateTask(id, data) {
     const task = this.fetchTaskById(id);
 
@@ -19,13 +21,15 @@ export default new TaskService({
       if (data != null && !!data.status) {
         task.status = data.status;
       } else {
-        if (task.status === "pending") task.status = "completed";
-        else task.status = "pending";
+        if (task.status === 'pending') task.status = 'completed';
+        else task.status = 'pending';
 
         // check if all the new status is "completed". If so, complete all subtasks.
         task.subtasks.forEach((sub) => (sub.status = task.status));
       }
     }
+
+    return Promise.resolve();
   },
   deleteTaskById: (id) => {
     // find the index of the task, whose "id" matches the "id" parameter.
@@ -36,25 +40,29 @@ export default new TaskService({
       // start removing from "taskIndex", then remove only one item.
       taskList.splice(taskIndex, 1);
     }
+    return Promise.resolve();
   },
   createSubtask: (title, task) => {
     const maxId = Math.max(...task.subtasks.map((t) => t.id));
     task.subtasks.push(new Subtask({ title, todo_id: task.id, id: maxId + 1 }));
+    task.status = 'pending'; // update parent task status to "pending", since new subtask is pending by default.
+    return Promise.resolve(task);
   },
   updateSubtask(subtask) {
     const parentTask = this.fetchTaskById(subtask.todo_id);
 
     if (!!parentTask) {
       const isAllCompleted = () =>
-        parentTask.subtasks.every((sub) => sub.status === "completed");
+        parentTask.subtasks.every((sub) => sub.status === 'completed');
 
       // toggle subtask.
-      if (subtask.status === "pending") subtask.status = "completed";
-      else subtask.status = "pending";
+      if (subtask.status === 'pending') subtask.status = 'completed';
+      else subtask.status = 'pending';
 
       // check if all the subtasks are completed. If so, complete the parent task.
-      if (isAllCompleted()) parentTask.status = "completed";
-      else parentTask.status = "pending";
+      if (isAllCompleted()) parentTask.status = 'completed';
+      else parentTask.status = 'pending';
     }
-  }
+    return Promise.resolve();
+  },
 });
